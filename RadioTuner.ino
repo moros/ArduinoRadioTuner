1fb7f535d042e9b269cce6f5ef3232ab2ce43fe6
Doug Mason <androidsoong@gmail.com>
2018-06-30 23:03:10 -0500

    using digitalPinToInterrupt into of 0, as recommended by Arduino documentation.

--- RadioTuner.ino
+++ RadioTuner.ino
@@ -34,7 +34,7 @@ void setup()
 
     pinMode(pot, INPUT);
     pinMode(interruptPin, INPUT);
-    attachInterrupt(0, setFrequency, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(interruptPin), setFrequency, CHANGE);
 }
 
 void loop()
